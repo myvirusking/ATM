b0b71b7e9917df4e250c5b3303f99d89c7dc5d16
unknown <myvirusking@gmail.com>
2018-07-14 20:48:22 +0530

    Changes to be committed: 	modified:   atm.sql

--- atm.sql
+++ atm.sql
@@ -13,6 +13,17 @@ SET time_zone = "+00:00";
 -- Database: `atm`
 --
 
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_account` (IN `name` VARCHAR(50), IN `account_number` INT, IN `pin` INT, IN `address` VARCHAR(100), IN `mobile_no` INT, IN `ifsc_code` VARCHAR(11))  begin
+    insert into account_holder(name,account_number,pin,address,mobile_no,ifsc_code)
+    values (name,account_number,pin,address,mobile_no,ifsc_code);
+    end$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -26,8 +37,25 @@ CREATE TABLE `account_holder` (
   `pin` int(11) NOT NULL,
   `address` varchar(100) NOT NULL,
   `mobile_no` int(11) NOT NULL,
-  `ifsc_code` varchar(11) NOT NULL
+  `ifsc_code` varchar(11) NOT NULL,
+  `last_deposit` int(11) DEFAULT NULL,
+  `last_withdraw` int(11) DEFAULT NULL,
+  `last_status` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `account_holder`
+--
+DELIMITER $$
+CREATE TRIGGER `after_update_account_holder` AFTER UPDATE ON `account_holder` FOR EACH ROW begin
+    IF new.last_status = 'withdraw' THEN
+    INSERT into transaction(account_number,current_balance,withdraw_amount) values(old.account_number,new.total_balance,new.last_withdraw);
+    ELSEIF new.last_status = 'deposit' THEN
+    INSERT into transaction(account_number,current_balance,deposit_amount) values(old.account_number,new.total_balance,new.last_deposit);
+    END IF;
+    end
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -38,8 +66,8 @@ CREATE TABLE `account_holder` (
 CREATE TABLE `transaction` (
   `account_number` int(11) NOT NULL,
   `current_balance` int(11) NOT NULL,
-  `withdraw_amount` int(11) NOT NULL,
-  `deposit_amount` int(11) NOT NULL,
+  `withdraw_amount` int(11) DEFAULT NULL,
+  `deposit_amount` int(11) DEFAULT NULL,
   `transaction_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
